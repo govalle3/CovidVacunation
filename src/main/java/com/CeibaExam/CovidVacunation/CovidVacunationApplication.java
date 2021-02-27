6f0d166f30c5abc4a93be52e5b49a176b71cc88d
German Ovalle Silva <39679249+govalle3@users.noreply.github.com>
2021-02-27 17:16:33 -0500

    Construccion de paquetes y dependencias

---
+++ src/main/java/com/CeibaExam/CovidVacunation/CovidVacunationApplication.java
@@ -0,0 +1,13 @@
+package com.CeibaExam.CovidVacunation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidVacunationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidVacunationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CeibaExam/CovidVacunation/CovidVacunationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CeibaExam.CovidVacunation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidVacunationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
