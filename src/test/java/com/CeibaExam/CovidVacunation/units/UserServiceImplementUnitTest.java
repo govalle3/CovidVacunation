13e7a5a21d6587ebb1da4a43154faa83f8dcdb12
German Ovalle Silva <39679249+govalle3@users.noreply.github.com>
2021-03-01 17:12:22 -0500

    Se arregla un problema en el test de minor age que se tuvo al momento de la presentacion grupal

--- src/test/java/com/CeibaExam/CovidVacunation/units/UserServiceImplementUnitTest.java
+++ src/test/java/com/CeibaExam/CovidVacunation/units/UserServiceImplementUnitTest.java
@@ -3,7 +3,6 @@ package com.CeibaExam.CovidVacunation.units;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 import com.CeibaExam.CovidVacunation.model.domain.User;
 import com.CeibaExam.CovidVacunation.model.repository.UserRepository;
 import com.CeibaExam.CovidVacunation.model.repository.builder.UserBuilder;
@@ -12,7 +11,6 @@ import java.util.Date;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,29 +23,29 @@ public class UserServiceImplementUnitTest {
   @Mock
   private UserBuilder userBuilder;
   private UserServiceImplement userServiceImplement;
+  private User user;
 
   @Before
   public void setup() {
     MockitoAnnotations.initMocks(this);
     this.userServiceImplement = new UserServiceImplement(userRepository, userBuilder);
+    this.user = mock(User.class);
   }
 
   @Test
   public void testExistingId() {
     //Arrange
-    when(userRepository.existsById(anyLong())).thenReturn(Boolean.TRUE);
-    User user = new User();
+    when(user.getNationalId()).thenReturn(5L);
+    when(userRepository.existsById(5L)).thenReturn(Boolean.TRUE);
     //Act
     Boolean answer = userServiceImplement.save(user);
     //Assert
     Assert.assertFalse(answer);
-
   }
 
   @Test
   public void testMinorAge() {
     //Arrange
-    User user = mock(User.class);
     Date date = new Date("2015/02/26");
     when(user.getDob()).thenReturn(date);
     //ACt
@@ -60,7 +58,6 @@ public class UserServiceImplementUnitTest {
   @Test
   public void testMajorAge() {
     //Arrange
-    User user = mock(User.class);
     Date date = new Date("2000/02/26");
     when(user.getDob()).thenReturn(date);
     //ACt
